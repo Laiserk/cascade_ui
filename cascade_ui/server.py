983648bf90c45098b4ccb50e5daea8d883eb89b1
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2023-09-02 15:59:45 +0300

    Add logging and path argument pasing

--- cascade_ui/server.py
+++ cascade_ui/server.py
@@ -1,9 +1,15 @@
 import os
 import uvicorn
+from argparse import ArgumentParser
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+import logging
+
 
 SCRIPT_DIR = os.path.dirname(__file__)
+
+logging.basicConfig(level="INFO")
+logger = logging.getLogger(__file__)
 
 app = FastAPI()
 
@@ -21,5 +27,17 @@ def repos():
     return HTMLResponse()
 
 
+parser = ArgumentParser()
+parser.add_argument(
+    "--path", type=str, help="Path of the workspace where to start UI", default="."
+)
+args = parser.parse_args()
+
+cwd = os.path.abspath(args.path)
+
 if __name__ == "__main__":
+    logger.info(f"Starting in {cwd}")
+    if not os.path.exists(cwd):
+        raise FileNotFoundError(f"The path you passed {cwd} does not exists")
+
     uvicorn.run(app)
