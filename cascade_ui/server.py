5ce23f0331dbd354cfe961d32ae6bb9eb4b95e1a
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2023-09-02 16:36:56 +0300

    Make server object and add workspace checks

--- cascade_ui/server.py
+++ cascade_ui/server.py
@@ -1,59 +1,71 @@
-"""
-Copyright 2023 Oleg Sevostyanov, Ilia Moiseev
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-
-import os
-import uvicorn
 from argparse import ArgumentParser
+import glob
+import logging
+import os
+
+from cascade.base import MetaHandler, supported_meta_formats
+from cascade import models as cdm
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-import logging
+import uvicorn
 
 
 SCRIPT_DIR = os.path.dirname(__file__)
 
-logging.basicConfig(level="INFO")
-logger = logging.getLogger(__file__)
 
-app = FastAPI()
+class Server:
+    def __init__(self, path: str) -> None:
+        meta_paths = glob.glob(os.path.join(path, "meta.*"))
+        meta_paths = [
+            path
+            for path in meta_paths
+            if os.path.splitext(path)[-1] in supported_meta_formats
+        ]
 
+        if len(meta_paths) != 1:
+            raise ValueError(f"There are {len(meta_paths)} meta objects in {path}")
+        else:
+            meta = MetaHandler.read(meta_paths[0])
 
-@app.get("/")
-def root() -> HTMLResponse:
-    root = os.path.join(os.path.dirname(SCRIPT_DIR), "web", "index.html")
-    with open(root, "r") as f:
-        html = f.read()
-    return HTMLResponse(content=html)
+        if not isinstance(meta, list):
+            raise ValueError(f"Metadata in {path} is not a list")
 
+        meta_type = meta[0].get("type")
+        if not meta_type:
+            raise ValueError(f"No type key in meta in {path}")
 
-@app.get("/repos")
-def repos():
-    return HTMLResponse()
+        if meta_type != "workspace":
+            raise ValueError(f"Cannot start UI in {type}, workspace only")
 
+        self._ws_meta = meta
 
-parser = ArgumentParser()
-parser.add_argument(
-    "--path", type=str, help="Path of the workspace where to start UI", default="."
-)
-args = parser.parse_args()
+    async def root_page(self) -> HTMLResponse:
+        root = os.path.join(os.path.dirname(SCRIPT_DIR), "web", "index.html")
+        with open(root, "r") as f:
+            html = f.read()
+        return HTMLResponse(content=html)
 
-cwd = os.path.abspath(args.path)
+    async def repos_page(self):
+        return HTMLResponse()
+
 
 if __name__ == "__main__":
+    logging.basicConfig(level="INFO")
+    logger = logging.getLogger(__file__)
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--path", type=str, help="Path of the workspace where to start UI", default="."
+    )
+    args = parser.parse_args()
+
+    cwd = os.path.abspath(args.path)
     logger.info(f"Starting in {cwd}")
-    if not os.path.exists(cwd):
-        raise FileNotFoundError(f"The path you passed {cwd} does not exists")
+
+    server = Server(cwd)
+
+    app = FastAPI()
+    app.add_api_route("/", server.root_page, methods=["get"])
+    app.add_api_route("/repos", server.repos_page, methods=["get"])
 
     uvicorn.run(app)
