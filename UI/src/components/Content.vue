cd530fef4cd9f3fe2102da664415a2ff912990ee
Oleg Sevostyanov <savosarenskyrim@gmail.com>
2024-03-02 21:23:27 +0500

    Proto You just need to fix the styles and move the list elements into a separate component

---
+++ UI/src/components/Content.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="content">
+    <slot/>
+  </div>
+
+</template>
+
+<style scoped>
+.content {
+  position: absolute;
+  left: 118px;
+  top: 109px;
+}
+</style>
--- UI/src/views/HomeView.vue
+++ UI/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import NavBar from "../components/NavBar.vue";
+import Content from "@/components/Content.vue";
 </script>
 
 <template>
@@ -7,8 +8,104 @@ import NavBar from "../components/NavBar.vue";
     <meta charset="utf-8"/>
     <title>List of experiments</title>
     <link rel="stylesheet" href="src/assets/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet">
   </head>
   <body>
   <NavBar/>
+  <Content>
+    <div class="title">
+      <p>Welcome to Cascade!</p>
+    </div>
+    <div class="listRepos">
+      <ul>
+        <li>
+          <a>
+            <div>
+              <p class="label">repo_1</p>
+              <p class="sub_label">01-09-2023</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a>
+            <div>
+              <p class="label">repo_2</p>
+              <p class="sub_label">01-09-2023</p>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a>
+            <div>
+              <p class="label">repo_3</p>
+              <p class="sub_label">01-09-2023</p>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </Content>
   </body>
 </template>
+<style scoped>
+
+.title {
+  font-weight: 700;
+  font-style: normal;
+  font-family: Montserrat, serif;
+  font-size: 40px;
+  color: #084C61;
+  line-height: 49px
+}
+
+.listRepos ul {
+  list-style-type: none;
+}
+
+.listRepos li {
+  float: left;
+}
+
+.listRepos li a div p {
+}
+
+.listRepos li a div {
+  width: 560px;
+  height: 248px;
+  background: #FFFFFF;
+  box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 27px;
+  margin-right: 58px;
+}
+
+.listRepos li a:hover {
+
+}
+
+
+.label {
+
+  font-family: 'Montserrat', serif;
+  font-style: normal;
+  font-weight: 700;
+
+  font-size: 40px;
+  line-height: 48px;
+
+  color: #084C61;
+}
+
+.sub_label {
+
+  font-family: 'Montserrat', serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 24px;
+
+  color: #919191;
+}
+
+
+</style>
