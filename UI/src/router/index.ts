111325a1b78243091d012ae35ab8aea3603fcd8b
Oleg Sevostyanov <savosarenskyrim@gmail.com>
2024-05-30 21:45:59 +0500

    Revert home route

--- UI/src/router/index.ts
+++ UI/src/router/index.ts
@@ -7,7 +7,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/main',
+      path: '/',
       name: 'home',
       component: HomeView
     },
