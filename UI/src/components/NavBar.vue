811582a062434e8609b25ba7804310474ae82209
Oleg Sevostyanov <savosarenskyrim@gmail.com>
2024-08-09 22:54:26 +0500

    mini fix

--- UI/src/components/NavBar.vue
+++ UI/src/components/NavBar.vue
@@ -1,15 +1,14 @@
-<script setup lang="ts">
-import HeadBar from "@/components/HeadBar.vue";
+<script lang="ts" setup>
+import HeadBar from "@/components/HeadBar.vue"
 </script>
 
 <template>
   <head>
     <meta charset="utf-8"/>
     <title>List of experiments</title>
-    <link rel="stylesheet" href="src/assets/main.css">
   </head>
   <body>
-  <nav class="navbar">
+  <nav>
     <HeadBar/>
     <!--    <SideBar/>-->
   </nav>
