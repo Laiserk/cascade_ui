8f5fc5b5d1241916cbe1ed5fa8f1fc2c38f20d42
Ilia Moiseev <villeman.5@yandex.ru>
2025-04-20 20:02:22 +0300

    Add simple lines table

--- UI/src/views/RepoView.vue
+++ UI/src/views/RepoView.vue
@@ -28,6 +28,15 @@ onMounted(async () => {
 
 const workspaceName = computed(() => workspace.value?.name ?? '');
 
+// Table headers for lines
+const lineHeaders = [
+  { title: 'Name', value: 'name' },
+  { title: 'Type', value: 'type' },
+  { title: 'Created At', value: 'created_at' },
+  { title: 'Updated At', value: 'updated_at' },
+  { title: 'Len', value: 'len' },
+];
+
 </script>
 
 <template>
@@ -38,6 +47,10 @@ const workspaceName = computed(() => workspace.value?.name ?? '');
       <p>Workspace: {{ workspaceName }}</p>
       <p v-if="repo">Repo: {{ repo.name }} ({{ repo.len }} lines)</p>
       <p v-else>Loading repo...</p>
+    </div>
+    <div v-if="repo && repo.lines">
+      <v-data-table :headers="lineHeaders" :items="repo.lines" class="mt-4">
+      </v-data-table>
     </div>
   </div>
 </template>
