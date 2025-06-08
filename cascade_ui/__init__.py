b431e0b229c7828b23218d46b2b638facbc3674f
Ilia Moiseev <villeman.5@yandex.ru>
2025-06-08 14:00:32 +0300

    Bump version

--- cascade_ui/__init__.py
+++ cascade_ui/__init__.py
@@ -14,6 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.1"
 __author__ = "Oleg Sevostyanov,Ilia Moiseev"
 __author_email__ = "ilia.moiseev.5@yandex.ru"
